7961af6cc17fda0fbdd7980b41e156298dafbc11
Jerit George <jerit.george@swri.org>
2021-03-03 18:34:53 -0600

    Add digital output service server

--- exercises/Perception-Driven_Manipulation/ros2/template_ws/src/robot_io/src/nodes/digital_output_update_server.cpp
+++ exercises/Perception-Driven_Manipulation/ros2/template_ws/src/robot_io/src/nodes/digital_output_update_server.cpp
@@ -13,91 +13,83 @@ static const std::string OUTPUT_TOPIC = "/digital_outputs";
 static const std::string OUTPUT_SERVICE = "/digital_output_update";
 static const std::string SERVER_RUNNING_PARAM = "/digital_output_server_running";
 
-class DigitalOutputUpdateServer
+class DigitalOutputUpdateServer : public rclcpp::Node
 {
 public:
 
-	DigitalOutputUpdateServer()
+	DigitalOutputUpdateServer() : Node("digital_output_update_server")
 	{
+		std::string nodeName = this->get_name();
 
+	    RCLCPP_INFO_STREAM(this->get_logger(), nodeName<<": Grasp execution action node started");
+		pub_ = this->create_publisher<soem_beckhoff_drivers::msg::DigitalMsg>(OUTPUT_TOPIC, 1);
+
+		while ( pub_.getNumSubscribers() <= 0 && rclcpp::ok())
+		{
+			rclcpp::Duration(5.0).sleep();
+		    RCLCPP_INFO_STREAM(this->get_logger(), nodeName<<": Waiting for digital output subscribers");
+		}
+
+		// initializing and sending output message
+		output_msg_.values.resize(robot_io::srv::DigitalOutputUpdate::Request::COUNT, false);
+		output_msg_.values[robot_io::srv::DigitalOutputUpdate::Request::COLLISION] = true;
+		pub_->publish(output_msg_);
+	    RCLCPP_INFO_STREAM(this->get_logger(), nodeName<<": Turning on air pressure to collision sensor");
+
+		server_ = this->create_server<(OUTPUT_SERVICE,&DigitalOutputUpdateServer::serviceCallback,this);
+		RCLCPP_INFO_STREAM(this->get_logger(), nodeName<<": Advertising "<<OUTPUT_SERVICE<<" service");
+
+		this->set_parameter(SERVER_RUNNING_PARAM,true);
 	}
 
 	~DigitalOutputUpdateServer()
 	{
 		// set param to false
-		ros::NodeHandle nh("/");
 		bool serverRunning = false;
-		if(nh.getParam(SERVER_RUNNING_PARAM,serverRunning) && serverRunning)
+		if(this->get_parameter(SERVER_RUNNING_PARAM,serverRunning) && serverRunning)
 		{
-			nh.setParam(SERVER_RUNNING_PARAM,false);
+			this->set_parameter(SERVER_RUNNING_PARAM,false);
 		}
 	}
 
-	bool serviceCallback(robot_io::DigitalOutputUpdate::Request &req,robot_io::DigitalOutputUpdate::Response &res)
+	bool serviceCallback(const std::shared_ptr<robot_io::DigitalOutputUpdate::Request> req,
+						std::shared_ptr<robot_io::DigitalOutputUpdate::Response> res)
 	{
 		if(validateRequest(req))
 		{
-			output_msg_.values[req.bit_index] = (bool)req.output_bit_state;
+			output_msg_.values[req->bit_index] = (bool)req->output_bit_state;
 			pub_.publish(output_msg_);
 
-			ros::Duration(0.5f).sleep();
-			res.output_bit_array = output_msg_.values;
+			rclcpp::Duration(0.5f).sleep();
+			res->output_bit_array = output_msg_.values;
 			return true;
 		}
 		else
 		{
-			ROS_ERROR_STREAM(ros::this_node::getName()<<": Rejected output update request");
+			RCLCPP_ERROR_STREAM(this->get_logger(), this->get_name()<<": Rejected output update request");
 			return false;
 		}
 	}
 
-	void init()
-	{
-		ros::NodeHandle nh("/");
-	    std::string nodeName = ros::this_node::getName();
-
-	    ROS_INFO_STREAM(nodeName<<": Grasp execution action node started");
-		pub_ = nh.advertise<soem_beckhoff_drivers::DigitalMsg>(OUTPUT_TOPIC, 1);
-
-		while ( pub_.getNumSubscribers() <= 0 && ros::ok())
-		{
-			ros::Duration(5.0).sleep();
-		    ROS_INFO_STREAM(nodeName<<": Waiting for digital output subscribers");
-		}
-
-		// initializing and sending output message
-		output_msg_.values.resize(robot_io::DigitalOutputUpdate::Request::COUNT, false);
-		output_msg_.values[robot_io::DigitalOutputUpdate::Request::COLLISION] = true;
-		pub_.publish(output_msg_);
-	    ROS_INFO_STREAM(nodeName<<": Turning on air pressure to collision sensor");
-
-		server_ = nh.advertiseService(OUTPUT_SERVICE,&DigitalOutputUpdateServer::serviceCallback,this);
-		ROS_INFO_STREAM(nodeName<<": Advertising "<<OUTPUT_SERVICE<<" service");
-
-		nh.setParam(SERVER_RUNNING_PARAM,true);
-	}
-
 	static bool checkSingletonInstanceRunning()
 	{
-		ros::NodeHandle nh("/");
 		bool server_running = false;
-		nh.getParam(SERVER_RUNNING_PARAM,server_running);
+		this->get_parameter(SERVER_RUNNING_PARAM,server_running);
 		return server_running;
 	}
 
 protected:
 
 	  // ros comm
-	  ros::NodeHandle node_;
-	  ros::ServiceServer server_;
-	  ros::Publisher pub_;
+	  rclcpp::Service<robot_io::srv::DigitalOutputUpdate>::SharedPtr server_;
+	  rclcpp::Publisher<soem_beckhoff_drivers::msg::DigitalMsg>::SharedPtr pub_;
 
 	  // messages
-	  soem_beckhoff_drivers::DigitalMsg output_msg_;
+	  soem_beckhoff_drivers::msg::DigitalMsg output_msg_;
 
-	  bool validateRequest(const robot_io::DigitalOutputUpdate::Request &req)
+	  bool validateRequest(const std::shared_ptr<robot_io::DigitalOutputUpdate::Request> req)
 	  {
-		  if(req.bit_index >= req.COUNT|| req.bit_index == req.COLLISION)
+		  if(req->bit_index >= req->COUNT|| req.bit_index == req->COLLISION)
 		  {
 			  return false;
 		  }
